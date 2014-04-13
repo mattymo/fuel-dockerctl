c9ef9fe09ce17a4e5211ae6577c4a1c201e3757d
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-13 17:18:15 +0400

    updated volume links

--- disable-services.sh
+++ disable-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-system_services="cobblerd httpd xinetd postgresql rabbitmq-server nginx dnsmasq rsyslog"
+system_services="cobblerd httpd xinetd postgresql rabbitmq-server nginx dnsmasq rsyslog mcollective"
 supervisord_services="assassind nailgun naily ostf receiverd"
 
 for system_service in $system_services; do
@@ -10,3 +10,5 @@ done
 for supervisord_service in $supervisord_services; do
   supervisorctl stop $supervisord_service
 done
+#Because rabbitmq-server doesn't really stop correctly
+pkill -u rabbitmq
--- sources/storage-puppet/Dockerfile
+++ sources/storage-puppet/Dockerfile
@@ -1,4 +1,6 @@
+#run with -v /etc/puppet:/puppet
 FROM busybox
+RUN mkdir -p /etc/puppet/modules /puppet
 ADD etc /etc
 VOLUME ["/etc/puppet"]
-CMD /bin/echo storage/puppet I am a data-only container for Fuel
+CMD /bin/echo storage/puppet I am a data-only container for Fuel && ln -s /puppet /etc/puppet
